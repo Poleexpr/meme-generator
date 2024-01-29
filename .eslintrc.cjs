86d8c84923308bede0d28f0711ad11a65146cc40
Anya Cherkasova <a.c4erkasova@gmail.com>
2024-01-29 15:14:55 +0300

    refactor: add a noImplicitAny to a tsconfig and rules to a eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,5 +14,8 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    "@typescript-eslint/no-unused-vars": "error",
+    // to enforce using type for object type definitions, can be type or interface 
+    "@typescript-eslint/consistent-type-definitions": ["error", "type"],
   },
 }
